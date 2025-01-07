b06410678a1e36a383caf875a53fcf81c95f8c9b
ngacho <brandonngacho@gmail.com>
2025-01-07 14:47:59 +0530

    successful deploy but still 404

--- function_app.py
+++
@@ -1,4 +0,0 @@
-import azure.functions as func
-from main import fastapi_app
-
-app = func.AsgiFunctionApp(app=fastapi_app, http_auth_level=func.AuthLevel.ANONYMOUS)
--- ingredients/__init__.py
+++ ingredients/__init__.py
@@ -1,10 +1,12 @@
-import os
-import fastapi
 from typing import List
-from ingredient import Ingredient
-from ingredient_parser import parse_ingredients_crf
+import azure.functions as func
+import os
+from fastapi import FastAPI
 
-fastapi_app = fastapi.FastAPI()
+from ingredients.ingredient import Ingredient
+from ingredients.ingredient_parser import parse_ingredients_crf
+
+fastapi_app = FastAPI()
 
 # Define the path to the CRF model relative to the script's location
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -14,29 +16,8 @@ MODEL_FILE_PATH = os.path.join(SCRIPT_DIR, "models/model-0601.crfmodel")
 async def read_root():
     return {"Hello": "World"}
 
-@fastapi_app.get("/sample")
-async def index():
-    return {
-        "info": "Try /hello/Shivani for parameterized route.",
-    }
-
-
-@fastapi_app.get("/hello/{name}")
-async def get_name(name: str):
-    return {
-        "name": name,
-    }
-
-@fastapi_app.put("/v2/ingredients")
-async def parse_ingredients_endpoint(ingredients: List[Ingredient]):
-    # Convert ingredients to strings
-    sentences = [str(ingredient) for ingredient in ingredients]
-    # Parse ingredients using the CRF model
-    parsed_response = parse_ingredients_crf(sentences, MODEL_FILE_PATH)
-    
-    return parsed_response
-
-@fastapi_app.put("/v2/iteration/ingredients")
+"""
+@fastapi_app.put("/")
 async def parse_ingredients_endpoint(ingredients: List[Ingredient]):
     parsed_responses = []  # List to store individual parsed responses
         
@@ -51,3 +32,7 @@ async def parse_ingredients_endpoint(ingredients: List[Ingredient]):
         parsed_responses.append(parsed_response)
     
     return parsed_responses
+"""
+
+async def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    return await func.AsgiMiddleware(fastapi_app).handle_async(req, context)
